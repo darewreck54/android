73b42fed59f00bd1c3a7950d64d395257052e34a
Derek Hang <d_hang10@hotmail.com>
2017-03-11 15:11:22 -0800

    support using OkHttp

--- week1/Flicks/app/src/main/java/com/codepath/flicks/activities/MainActivity.java
+++ week1/Flicks/app/src/main/java/com/codepath/flicks/activities/MainActivity.java
@@ -3,24 +3,28 @@ package com.codepath.flicks.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.codepath.flicks.R;
 import com.codepath.flicks.adapters.MovieAdapter;
 import com.codepath.flicks.models.Movie;
 import com.codepath.flicks.network.MovieDbClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-import cz.msebera.android.httpclient.Header;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private MovieDbClient client;
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private MovieAdapter adapterMovies;
     private final int DETAIL_MOVIE_REQUEST_CODE = 30;
     private ArrayList<Movie> aMovies;
+    private final String TAG = MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,29 +59,41 @@ public class MainActivity extends AppCompatActivity {
 
     private void fetchBoxOfficeMovies() {
         client = new MovieDbClient();
-        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
+        client.getBoxOfficeMovies(new Callback() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(getApplicationContext(), "Failed to retrieve movies. Please try again!", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
                 try {
-                    JSONArray items = response.getJSONArray("results");
-                    final ArrayList<Movie> movies = Movie.fromJsonArray(items);
+                    if(response.isSuccessful()) {
+                        String responseData = response.body().string();
+                        JSONObject json = new JSONObject(responseData);
 
-
-                    for (Movie movie : movies) {
-
-
-                        adapterMovies.add(movie); // add movie through the adapter
+                        JSONArray items = json.getJSONArray("results");
+                        final ArrayList<Movie> movies = Movie.fromJsonArray(items);
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                for (Movie movie : movies) {
+                                    adapterMovies.add(movie); // add movie through the adapter
+                                }
+                                adapterMovies.notifyDataSetChanged();
+                            }
+                        });
                     }
-                    adapterMovies.notifyDataSetChanged();
+                    else {
+                        Log.e(TAG, "Error: " + response.message() + " " + response.body().toString());
+                    }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
+
         });
     }
 }

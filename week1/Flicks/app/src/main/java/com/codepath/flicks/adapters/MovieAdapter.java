7bff340bfed6c07e66a821a20de3a4936045f951
Derek Hang <d_hang10@hotmail.com>
2017-03-12 10:09:26 -0700

    fix issue with butterknife related to view holder pattern.  Layout ids were referenced incorrectly

--- week1/Flicks/app/src/main/java/com/codepath/flicks/adapters/MovieAdapter.java
+++ week1/Flicks/app/src/main/java/com/codepath/flicks/adapters/MovieAdapter.java
@@ -2,6 +2,8 @@ package com.codepath.flicks.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +19,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.Optional;
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by darewreck_PC on 3/8/2017.
@@ -31,30 +34,34 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
     public static class ViewHolder {
         @BindView(R.id.tv_title) TextView title;
         @BindView(R.id.tv_overview) TextView overview;
-        @BindView(R.id.ivPosterImage) ImageView poster;
-
+        @BindView(R.id.iv_posterImage) ImageView poster;
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
         }
     }
     public static class ViewHolder2 {
-        @BindView(R.id.ivPosterImage) ImageView poster1;
-
+        @BindView(R.id.iv_posterImage) ImageView poster1;
         public ViewHolder2(View view) {
             ButterKnife.bind(this, view);
         }
     }
 
     public MovieAdapter(Context context, List<Movie> movies) {
-        super(context, android.R.layout.simple_list_item_1);
+        super(context, 0, movies);
     }
 
     private enum LayoutType{
-        UNKNOWN,
         NORMAL,
         POPULAR
     }
     private final int POPULAR_STAR_RATING = 5;
+
+    @Override
+    public int getViewTypeCount() {
+        // Returns the number of types of Views that will be created by this adapter
+        // Each type represents a set of views that can be converted
+        return LayoutType.values().length;
+    }
 
     // Return an integer representing the type by fetching the enum type ordinal
     @Override
@@ -62,7 +69,7 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         return (getItem(position).getVoteAverage() > POPULAR_STAR_RATING)? LayoutType.POPULAR.ordinal(): LayoutType.NORMAL.ordinal();
     }
 
-    @Optional
+    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item type for this position
@@ -77,6 +84,7 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
             if(convertView == null) {
                 convertView = getInflatedLayoutForType(type, parent);
                 viewHolder = new ViewHolder(convertView);
+
 
                 convertView.setTag(viewHolder);
             } else {
@@ -105,6 +113,7 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
             if(convertView == null) {
                 convertView = getInflatedLayoutForType(type, parent);
                 viewHolder = new ViewHolder2(convertView);
+
                 convertView.setTag(viewHolder);
             } else {
                 viewHolder = (ViewHolder2) convertView.getTag();
@@ -123,12 +132,17 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
 
     // Given the item type, responsible for returning the correct inflated XML layout file
     private View getInflatedLayoutForType(int type, ViewGroup parent) {
-        if (type == LayoutType.NORMAL.ordinal()) {
-            return LayoutInflater.from(getContext()).inflate(R.layout.item_box_office_movie, parent, false);
-        } else if (type == LayoutType.POPULAR.ordinal()) {
-            return LayoutInflater.from(getContext()).inflate(R.layout.item_box_office_movie_popular, parent, false);
-        } else {
-            return null;
+        View selectedView = null;
+
+        if (type == 0) {
+            selectedView =  LayoutInflater.from(getContext()).inflate(R.layout.item_box_office_movie, parent, false);
+        } else if (type == 1) {
+            selectedView =  LayoutInflater.from(getContext()).inflate(R.layout.item_box_office_movie_popular, parent, false);
         }
+        else {
+            Log.e(TAG, "Layout Type is Invalid");
+        }
+
+        return selectedView;
     }
 }

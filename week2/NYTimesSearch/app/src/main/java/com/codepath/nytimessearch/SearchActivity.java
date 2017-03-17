2b94a09f13a96d047f01af964eff8c0bea1e5e29
Derek Hang <dehang@microsoft.com>
2017-03-17 07:33:53 -0700

    addition of week2 project

---
+++ week2/NYTimesSearch/app/src/main/java/com/codepath/nytimessearch/SearchActivity.java
@@ -0,0 +1,19 @@
+package com.codepath.nytimessearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+
+    public void onArticleSearch(View view) {
+
+    }
+}

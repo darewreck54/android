1d21d61e68eb07f3bfb2c6cf3bc53a4e848517ab
Derek Hang <dehang@microsoft.com>
2017-03-26 02:41:15 -0700

    initial base checkin

---
+++ week3/SimpleTweets/app/src/main/java/com/codepath/simpletweets/TwitterActivityTemp.java
@@ -0,0 +1,13 @@
+package com.codepath.simpletweets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterActivityTemp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_temp);
+    }
+}
